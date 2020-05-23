05916d5a5b61412aa66f6d2d866a69578dd55369
Zelly <zellifone@gmail.com>
2020-05-22 23:46:29 -0400

    Initial Commit

---
+++ Core/NetInteract.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerTargetPublicAPI.Core
+{
+    public class NetInteract
+    {
+        public string DnsLookup(string domain)
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://api.hackertarget.com/dnslookup/?q=" + domain);
+            client.Dispose();
+            return result;
+        }
+        public string GeoIp(string ip)
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://api.hackertarget.com/geoip/?q=" + ip);
+            client.Dispose();
+            return result;
+        }
+        public string TcpScan(string ip)
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://api.hackertarget.com/nmap/?q=" + ip);
+            client.Dispose();
+            return result;
+        }
+        public string ReverseIpLookup(string ip)
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://api.hackertarget.com/reverseiplookup/?q=" + ip);
+            client.Dispose();
+            return result;
+        }
+
+        public string ReverseDNSLookup(string ip)
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://api.hackertarget.com/reversedns/?q=" + ip);
+            client.Dispose();
+            return result;
+        }
+        public string SubNetLookup(string ip)
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://api.hackertarget.com/subnetcalc/?q=" + ip);
+            client.Dispose();
+            return result;
+        }
+        public string BannerLookup(string ip)
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://api.hackertarget.com/bannerlookup/?q=" + ip);
+            client.Dispose();
+            return result;
+        }
+        public string ASNLookup(string ip)
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://api.hackertarget.com/aslookup/?q=" + ip);
+            client.Dispose();
+            return result;
+        }
+        public string SharedServer(string ip)
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://api.hackertarget.com/findshareddns/?q=" + ip);
+            client.Dispose();
+            return result;
+        }
+        public string ZoneTransfer(string domain)
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://api.hackertarget.com/zonetransfer/?q=" + domain);
+            client.Dispose();
+            return result;
+        }
+        public string Whois(string domain)
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://api.hackertarget.com/whois/?q=" + domain);
+            client.Dispose();
+            return result;
+
+        }
+
+        public string Ping(string domain)
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://api.hackertarget.com/nping/?q=" + domain);
+            client.Dispose();
+            return result;
+        }
+        public string TraceRoute(string ip)
+        {
+            WebClient client = new WebClient();
+            string result = client.DownloadString("https://api.hackertarget.com/mtr/?q=" + ip);
+            client.Dispose();
+            return result;
+        }
+    }
+}
---
+++ HackerTargetClient.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerTargetPublicAPI
+{
+    public class HackerTargetClient
+    {
+
+        public enum MethodsProperties { DnsLookup, GeoIPLookup, TcpScan, ReverseIPLookup, ReverseDNSLookup, SubnetLookup, BannerLookup, ASNLookup, SharedDNS, ZoneTransfer, Whois, Ping, TraceRoute }
+        public static string GetInformation(MethodsProperties properties, string IpOrDomain)
+        {
+            string result = ValidateMethod(properties, IpOrDomain);
+            return result;
+        }
+        private static string ValidateMethod(MethodsProperties prop, string arg)
+        {
+            Core.NetInteract net = new Core.NetInteract();
+
+            if (prop == MethodsProperties.DnsLookup) { return net.DnsLookup(arg); }
+            if (prop == MethodsProperties.GeoIPLookup) { return net.GeoIp(arg); }
+            if (prop ==MethodsProperties.TcpScan) { return net.TcpScan(arg); }
+            if (prop == MethodsProperties.ReverseIPLookup) { return net.ReverseIpLookup(arg); }
+            if (prop == MethodsProperties.ReverseDNSLookup) { return net.ReverseDNSLookup(arg); }
+            if (prop == MethodsProperties.SubnetLookup) { return net.SubNetLookup(arg); }
+            if (prop == MethodsProperties.BannerLookup) { return net.BannerLookup(arg); }
+            if (prop == MethodsProperties.ASNLookup) { return net.ASNLookup(arg); }
+            if (prop == MethodsProperties.SharedDNS) { return net.SharedServer(arg); }
+            if (prop == MethodsProperties.ZoneTransfer) { return net.ZoneTransfer(arg); }
+            if (prop == MethodsProperties.Whois) { return net.Whois(arg); }
+            if (prop == MethodsProperties.Ping) { return net.Ping(arg); }
+            return "None";
+        }
+    }
+}
